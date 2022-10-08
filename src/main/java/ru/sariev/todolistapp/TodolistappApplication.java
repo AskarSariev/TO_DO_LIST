99c971fe29265719ba73b9dae938b37fd2a6e65c
Askar Sariev <askar-64@mail.ru>
2022-10-08 10:29:47 +0300

    Create project

---
+++ src/main/java/ru/sariev/todolistapp/TodolistappApplication.java
@@ -0,0 +1,13 @@
+package ru.sariev.todolistapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodolistappApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TodolistappApplication.class, args);
+    }
+
+}
